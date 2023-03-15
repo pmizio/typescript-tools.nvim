d5241607c266487ae8fa7f747e7ea3eeaf5bbb0f
KostkaBrukowa <jaroslaw.glegola@allegro.pl>
2023-03-15 18:44:38 +0100

    Small refactor

--- lua/typescript-tools/user_commands.lua
+++ lua/typescript-tools/user_commands.lua
@@ -2,23 +2,30 @@ local constants = require "typescript-tools.protocol.constants"
 
 local M = {}
 
-local function send_organize_import_request(skipDestructiveCodeActions)
-  return function()
-    vim.lsp.buf_request(
-      0,
-      constants.CustomMethods.OrganizeImports,
-      { file = vim.fn.expand "%p", skipDestructiveCodeActions = skipDestructiveCodeActions },
-      function(_, result)
-        if not result or not result.changes then
-          return
-        end
+--- @param workspace_edit table
+--- @return nil
+local function apply_workspace_edit(workspace_edit)
+  if not workspace_edit or not workspace_edit.changes then
+    return
+  end
 
-        vim.lsp.util.apply_workspace_edit(result, "utf-8")
-      end
-    )
+  vim.lsp.util.apply_workspace_edit(workspace_edit, "utf-8")
+end
+
+--- @param skipDestructiveCodeActions boolean
+--- @return function
+local function send_organize_import_request(skipDestructiveCodeActions)
+  local params =
+    { file = vim.fn.expand "%p", skipDestructiveCodeActions = skipDestructiveCodeActions }
+
+  return function()
+    vim.lsp.buf_request(0, constants.CustomMethods.OrganizeImports, params, function(_, result)
+      apply_workspace_edit(result)
+    end)
   end
 end
 
+--- @return nil
 M.setup_user_commands = function()
   vim.api.nvim_create_user_command(
     "TSToolsOrganizeImports",
