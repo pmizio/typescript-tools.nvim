d32d1dc9317e510779ae41e15462e971b8b49e11
KostkaBrukowa <jaroslaw.glegola@allegro.pl>
2023-03-16 11:08:46 +0100

    importUserCommands- | Fixes luacheck error

--- lua/typescript-tools/custom_handlers.lua
+++ lua/typescript-tools/custom_handlers.lua
@@ -2,6 +2,7 @@ local constants = require "typescript-tools.protocol.constants"
 local M = {}
 
 function M.setup_lsp_commands()
+  -- luacheck:ignore 122
   vim.lsp.commands[constants.CustomMethods.OrganizeImports] = function() end
 end
 
