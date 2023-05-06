8810ff6d996d6ad03390b3d76f763e40086bb981
Jarosław Glegoła <35625949+KostkaBrukowa@users.noreply.github.com>
2023-05-06 19:03:59 +0200

    feat: Improves definition and references requests handling

--- lua/typescript-tools/protocol/handlers/definition.lua
+++ lua/typescript-tools/protocol/handlers/definition.lua
@@ -1,6 +1,28 @@
 local constants = require "typescript-tools.protocol.constants"
 local utils = require "typescript-tools.protocol.utils"
 
+local file_span_with_context_to_location_link = function(file_span, text_span)
+  local originSelectionRange = text_span and utils.convert_tsserver_range_to_lsp(text_span) or nil
+  local target = {
+    uri = vim.uri_from_fname(file_span.file),
+    range = utils.convert_tsserver_range_to_lsp(file_span),
+  }
+  local targetRange = file_span.contextStart
+      and file_span.contextEnd
+      and utils.convert_tsserver_range_to_lsp {
+        start = file_span.contextStart,
+        ["end"] = file_span.contextEnd,
+      }
+    or target.range
+
+  return {
+    originSelectionRange = originSelectionRange,
+    targetRange = targetRange,
+    targetUri = target.uri,
+    targetSelectionRange = target.range,
+  }
+end
+
 -- tsserver protocol reference:
 -- https://github.com/microsoft/TypeScript/blob/7910c509c4545517489d6264571bb6c05248fb4a/lib/protocol.d.ts#L662
 local definition_request_handler = function(_, params)
@@ -10,9 +32,7 @@ local definition_request_handler = function(_, params)
     command = constants.CommandTypes.DefinitionAndBoundSpan,
     arguments = vim.tbl_extend("force", {
       file = vim.uri_to_fname(text_document.uri),
-    }, utils.convert_lsp_position_to_tsserver(
-      params.position
-    )),
+    }, utils.convert_lsp_position_to_tsserver(params.position)),
   }
 end
 
@@ -20,10 +40,7 @@ end
 -- https://github.com/microsoft/TypeScript/blob/7910c509c4545517489d6264571bb6c05248fb4a/lib/protocol.d.ts#L668
 local definition_response_handler = function(_, body)
   return vim.tbl_map(function(definition)
-    return {
-      uri = vim.uri_from_fname(definition.file),
-      range = utils.convert_tsserver_range_to_lsp(definition),
-    }
+    return file_span_with_context_to_location_link(definition, body.textSpan)
   end, body.definitions)
 end
 
