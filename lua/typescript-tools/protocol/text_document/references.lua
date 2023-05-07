4d8626724cf47a621509a7655a5e54ac501bde91
Pawel Mizio <pawel.mizio@allegro.pl>
2023-05-07 15:01:34 +0200

    feat: improves definition and references requests handling (#29)

--- lua/typescript-tools/protocol/text_document/references.lua
+++ lua/typescript-tools/protocol/text_document/references.lua
@@ -10,10 +10,17 @@ function M.handler(request, response, params)
   request(utils.tsserver_location_request(c.CommandTypes.References, params))
 
   local body = coroutine.yield()
+  local references = body.refs
+
+  if not params.context or not params.context.includeDeclaration then
+    references = vim.tbl_filter(function(reference)
+      return not reference.isDefinition
+    end, references)
+  end
 
   -- tsserver protocol reference:
   -- https://github.com/microsoft/TypeScript/blob/45148dd715a7c3776840778b4df41e7e0bd0bf12/lib/protocol.d.ts#L898
-  response(utils.tsserver_location_response(body.refs))
+  response(utils.tsserver_location_response(references))
 end
 
 return M
