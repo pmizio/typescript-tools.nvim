f0d701ae4972d92e50797bbe7b6b7994154d1a7c
Jarosław Glegoła <35625949+KostkaBrukowa@users.noreply.github.com>
2023-07-04 11:26:54 +0200

    refactor: moves tests to other file

--- tests/ts_project/src/batch_code_actions.ts
+++ tests/ts_project/src/batch_code_actions.ts
@@ -1,10 +1,10 @@
 async function foo() {
-  const y = 0;
+  const unused = 0;
   return 1;
 }
 
 function bar() {
-  const y = export1;
+  const unused = export1;
   const f = await foo();
   return f;
 }
