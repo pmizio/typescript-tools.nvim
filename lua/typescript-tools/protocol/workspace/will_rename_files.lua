2c6c63ea7b40dc1086aabe4508d223e39489437d
Jarosław Glegoła <35625949+KostkaBrukowa@users.noreply.github.com>
2023-07-12 16:36:17 +0200

    fix: will_rename_files handler for more than one file (#89)

--- lua/typescript-tools/protocol/workspace/will_rename_files.lua
+++ lua/typescript-tools/protocol/workspace/will_rename_files.lua
@@ -4,13 +4,10 @@ local utils = require "typescript-tools.protocol.utils"
 local M = {}
 
 ---@param code_edits table
----@return table<string, table>|nil
-local function make_text_edits(code_edits)
+local function add_text_edits(code_edits, changes)
   if not code_edits then
-    return nil
+    return
   end
-
-  local changes = {}
 
   for _, code_edit in ipairs(code_edits) do
     local uri = vim.uri_from_fname(code_edit.fileName)
@@ -26,35 +23,39 @@ local function make_text_edits(code_edits)
 
     changes[uri] = text_edits
   end
-
-  return changes
 end
 
 ---@type TsserverProtocolHandler
-function M.handler(request, response, params)
-  -- in params there could be multiple files but in order fulfill request
-  -- with multiple files we would need to send multiple requests to tsserver
-  -- at the moment there is no abstraction to do that so we just take first file
-  local first_file = params.files[1]
-  local old_file_path = vim.uri_to_fname(first_file.oldUri)
-  local new_file_path = vim.uri_to_fname(first_file.newUri)
+function M.handler(request, response, params, ctx)
+  local seqs = vim.tbl_map(function(file)
+    local old_file_path = vim.uri_to_fname(file.oldUri)
+    local new_file_path = vim.uri_to_fname(file.newUri)
 
-  -- tsserver protocol reference:
-  -- https://github.com/microsoft/TypeScript/blob/29cbfe9a2504cfae30bae938bdb2be6081ccc5c8/lib/protocol.d.ts#L511
-  request {
-    command = c.CommandTypes.GetEditsForFileRename,
-    arguments = {
-      oldFilePath = old_file_path,
-      newFilePath = new_file_path,
-    },
-  }
+    -- tsserver protocol reference:
+    -- https://github.com/microsoft/TypeScript/blob/29cbfe9a2504cfae30bae938bdb2be6081ccc5c8/lib/protocol.d.ts#L511
+    return request {
+      command = c.CommandTypes.GetEditsForFileRename,
+      arguments = {
+        oldFilePath = old_file_path,
+        newFilePath = new_file_path,
+      },
+    }
+  end, params.files)
 
-  local body = coroutine.yield()
+  ctx.synthetic_seq = table.concat(seqs, "_")
+
+  local changes = {}
+
+  for _ in ipairs(seqs) do
+    local body = coroutine.yield()
+
+    add_text_edits(body, changes)
+  end
 
   -- tsserver protocol reference:
   -- https://github.com/microsoft/TypeScript/blob/29cbfe9a2504cfae30bae938bdb2be6081ccc5c8/lib/protocol.d.ts#L511
   response {
-    changes = make_text_edits(body),
+    changes = changes,
   }
 end
 
