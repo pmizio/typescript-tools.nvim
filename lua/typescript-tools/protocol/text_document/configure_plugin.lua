75c61aa268bdc4f9da4ba58fb4e55e278df90fee
IzioDev <romain@izio.fr>
2023-08-06 01:11:58 +0200

    chore: wip

--- lua/typescript-tools/protocol/text_document/configure_plugin.lua
+++ lua/typescript-tools/protocol/text_document/configure_plugin.lua
@@ -3,7 +3,7 @@ local c = require "typescript-tools.protocol.constants"
 local M = {}
 
 ---@type TsserverProtocolHandler
-function M.handler(request, response, params)
+function M.handler(request, _, params)
   -- tsserver protocol reference:
   -- https://github.com/microsoft/TypeScript/blob/e14a2298c5add93816c6f487bcfc5ac72e3a4c59/lib/protocol.d.ts#L1493
   request {
@@ -13,15 +13,6 @@ function M.handler(request, response, params)
       configuration = params.configuration,
     },
   }
-
-  local body = coroutine.yield()
-
-  -- tsserver protocol reference:
-  -- https://github.com/microsoft/TypeScript/blob/e14a2298c5add93816c6f487bcfc5ac72e3a4c59/lib/protocol.d.ts#L1574
-  response(vim.tbl_map(function(unknown)
-    print(unknown)
-    return unknown
-  end, body))
 end
 
 return M
