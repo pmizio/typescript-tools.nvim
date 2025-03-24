600943fd8608053672449f33fcb82ea8b7f15b9a
Dmytro Soltys <soap@slotos.net>
2025-03-24 21:50:36 +0100

    fix: cache ftplugin on first load
    This eliminates repeated reads of ftplugin implementation file on
    repeated filetype events. The only reliably persistent store available
    to ftplugin scripts that I can think of is the global table, which is
    why I'm using it to store loaded ftplugin "object" in. The prefix is a
    string gneerated by rolling a fair dice in order to make name collision
    highly improbable.
    
    There might be a more efficient way to do this. Please tear this commit
    apart if you know of it.

--- ftplugin/javascript.lua
+++ ftplugin/javascript.lua
@@ -1,1 +1,8 @@
-dofile(vim.fn.expand "<sfile>:h:h" .. "/utils/ftplugin-common.lua")
+local ftplugin = _G.nnyIAsk5fJtqzUaJ_typescript_tools_ftplugin
+
+if not ftplugin then
+  ftplugin = dofile(vim.fn.expand "<sfile>:h:h" .. "/utils/ftplugin-common.lua")
+  _G.nnyIAsk5fJtqzUaJ_typescript_tools_ftplugin = ftplugin
+end
+
+ftplugin.initialize()
--- utils/ftplugin-common.lua
+++ utils/ftplugin-common.lua
@@ -1,64 +1,70 @@
-if vim.fn.exists "g:disable_typescript_tools" == 1 then
-  return
+local M = {}
+
+M.initialize = function()
+  if vim.fn.exists "g:disable_typescript_tools" == 1 then
+    return
+  end
+  if vim.fn.exists "b:did_typescript_tools_ftplugin" == 1 then
+    return
+  end
+  vim.b.did_typescript_tools_ftplugin = true
+
+  ---@param name string
+  ---@param fn function
+  local function create_command(name, fn)
+    local command_completion = {
+      nargs = "?",
+      complete = function()
+        return { "sync" }
+      end,
+    }
+    vim.api.nvim_buf_create_user_command(0, name, function(cmd)
+      local words = cmd.fargs
+
+      if #words == 1 and words[1] ~= "sync" then
+        vim.notify("No such command", vim.log.levels.ERROR)
+        return
+      end
+
+      fn(#words == 1)
+    end, command_completion)
+  end
+
+  create_command("TSToolsOrganizeImports", function(is_sync)
+    require("typescript-tools.api").organize_imports(is_sync)
+  end)
+
+  create_command("TSToolsSortImports", function(is_sync)
+    require("typescript-tools.api").sort_imports(is_sync)
+  end)
+
+  create_command("TSToolsRemoveUnusedImports", function(is_sync)
+    require("typescript-tools.api").remove_unused_imports(is_sync)
+  end)
+
+  create_command("TSToolsGoToSourceDefinition", function(is_sync)
+    require("typescript-tools.api").go_to_source_definition(is_sync)
+  end)
+
+  create_command("TSToolsRemoveUnused", function(is_sync)
+    require("typescript-tools.api").remove_unused(is_sync)
+  end)
+
+  create_command("TSToolsAddMissingImports", function(is_sync)
+    require("typescript-tools.api").add_missing_imports(is_sync)
+  end)
+
+  create_command("TSToolsFixAll", function(is_sync)
+    require("typescript-tools.api").fix_all(is_sync)
+  end)
+
+  create_command("TSToolsRenameFile", function(is_sync)
+    require("typescript-tools.api").rename_file(is_sync)
+  end)
+
+  create_command("TSToolsFileReferences", function(is_sync)
+    require("typescript-tools.api").file_references(is_sync)
+  end)
 end
-if vim.fn.exists "b:did_typescript_tools_ftplugin" == 1 then
-  return
-end
-vim.b.did_typescript_tools_ftplugin = true
 
----@param name string
----@param fn function
-local function create_command(name, fn)
-  local command_completion = {
-    nargs = "?",
-    complete = function()
-      return { "sync" }
-    end,
-  }
-  vim.api.nvim_buf_create_user_command(0, name, function(cmd)
-    local words = cmd.fargs
-
-    if #words == 1 and words[1] ~= "sync" then
-      vim.notify("No such command", vim.log.levels.ERROR)
-      return
-    end
-
-    fn(#words == 1)
-  end, command_completion)
-end
-
-create_command("TSToolsOrganizeImports", function(is_sync)
-  require("typescript-tools.api").organize_imports(is_sync)
-end)
-
-create_command("TSToolsSortImports", function(is_sync)
-  require("typescript-tools.api").sort_imports(is_sync)
-end)
-
-create_command("TSToolsRemoveUnusedImports", function(is_sync)
-  require("typescript-tools.api").remove_unused_imports(is_sync)
-end)
-
-create_command("TSToolsGoToSourceDefinition", function(is_sync)
-  require("typescript-tools.api").go_to_source_definition(is_sync)
-end)
-
-create_command("TSToolsRemoveUnused", function(is_sync)
-  require("typescript-tools.api").remove_unused(is_sync)
-end)
-
-create_command("TSToolsAddMissingImports", function(is_sync)
-  require("typescript-tools.api").add_missing_imports(is_sync)
-end)
-
-create_command("TSToolsFixAll", function(is_sync)
-  require("typescript-tools.api").fix_all(is_sync)
-end)
-
-create_command("TSToolsRenameFile", function(is_sync)
-  require("typescript-tools.api").rename_file(is_sync)
-end)
-
-create_command("TSToolsFileReferences", function(is_sync)
-  require("typescript-tools.api").file_references(is_sync)
-end)
+return M
