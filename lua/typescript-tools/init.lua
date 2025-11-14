6a2ed8ae154a9908831b64254d2b322dd86f4113
Dalius Dobravolskas <daliusd@wix.com>
2025-11-14 13:53:58 +0200

    fix: on_attach is ignored

--- lua/typescript-tools/init.lua
+++ lua/typescript-tools/init.lua
@@ -6,6 +6,8 @@ local M = {}
 
 ---@param config { settings?: table, config?: vim.lsp.Config }
 function M.setup(config)
+  config = config or {}
+
   local version_ok, version_msg = util.check_minimum_nvim_version()
   if not version_ok then
     vim.notify(string.format("[typescript-tools.nvim] %s", version_msg), vim.log.levels.ERROR)
@@ -15,8 +17,17 @@ function M.setup(config)
 
   plugin_config.load_settings(settings)
 
+  -- Extract LSP config options (on_attach, capabilities, etc.)
+  -- Filter out plugin-specific options like 'settings'
+  local lsp_config = {}
+  for key, value in pairs(config) do
+    if key ~= "settings" then
+      lsp_config[key] = value
+    end
+  end
+
   if vim.lsp.config[plugin_config.plugin_name] == nil then
-    vim.lsp.config[plugin_config.plugin_name] = vim.tbl_deep_extend("keep", config.config or {}, {
+    vim.lsp.config[plugin_config.plugin_name] = vim.tbl_deep_extend("keep", lsp_config, {
       cmd = function(...)
         return rpc.start(...)
       end,
